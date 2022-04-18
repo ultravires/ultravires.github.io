ef1199226d97388015f68bcd0fd57ca5923f771f
ultravires <x555666777@qq.com>
2022-04-18 17:31:14 +0800

    增加 菜单和轮播图的配置

--- config.js
+++ config.js
@@ -3,8 +3,26 @@ export default {
 
   menu: [
     {
+      icon: 'icon-home',
       name: '首页',
       path: '/'
+    },
+    {
+      icon: 'icon-about',
+      name: '关于',
+      path: '/about'
+    },
+    {
+      icon: 'icon-github',
+      name: 'GitHub',
+      path: 'https://www.github.com/ultravires'
+    }
+  ],
+
+  banner: [
+    {
+      title: '辞去归来兮',
+      image: '/public/images/happy.png'
     }
   ]
 };
--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-import config from './config';
+import config from '../config.js';
+import { Carousel, CarouselItem } from './components/carousel.js';
+import { Menu, MenuItem } from './components/menu.js';
+
+const menu = new Menu();
+for (let i = 0, len = config.menu.length; i < len; ++i) {
+  const menuItem = new MenuItem(config.menu[i]);
+  menu.append(menuItem);
+}
+document.getElementById('mainNavigation').append(menu);
+
+const carousel = new Carousel();
+for (let i = 0, len = config.banner.length; i < len; ++i) {
+  const carouselItem = new CarouselItem();
+  const image = document.createElement('img');
+  image.src = config.banner[i].image;
+  carouselItem.append(image);
+  carousel.append(carouselItem);
+}
+document.getElementById('banner').append(carousel);
