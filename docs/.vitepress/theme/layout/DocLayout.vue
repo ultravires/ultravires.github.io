7f865639679106e51483e04fda58dc4fecf0dc32
ultravires <x555666777@qq.com>
2024-05-20 11:56:26 +0800

    Update

--- docs/.vitepress/theme/layout/DocLayout.vue
+++ docs/.vitepress/theme/layout/DocLayout.vue
@@ -6,7 +6,7 @@ import VHeader from '../components/VHeader.vue';
 <template>
   <VHeader />
   <div class="container mx-auto py-12">
-    <article id="article" class="dark:bg-slate-950 dark:border-slate-800 dark:prose-invert prose-code:before:content-none prose-code:after:content-none prose-code:px-2 prose-code:py-1 prose-code:text-primary prose-code:text-opacity-90 prose-code:bg-primary prose-code:bg-opacity-10 prose-code:rounded-md prose-code:text-sm prose-blockquote:not-italic prose-blockquote:font-normal md:prose-lg lg:prose-xl prose prose-lg max-w-none p-12 mx-auto bg-white rounded-md border border-solid border-slate-100 shadow-sm">
+    <article id="article" class="dark:bg-slate-950 dark:border-slate-800 dark:prose-pre:border-slate-800 dark:prose-invert prose-pre:border prose-pre:border-solid prose-pre:border-gray-100 prose-code:before:content-none prose-code:after:content-none prose-code:px-2 prose-code:py-1 prose-code:text-primary prose-code:text-opacity-90 prose-code:bg-primary prose-code:bg-opacity-10 prose-code:rounded-md prose-code:text-sm prose-blockquote:not-italic prose-blockquote:font-normal md:prose-lg lg:prose-xl prose prose-lg max-w-none p-12 mx-auto bg-white rounded-md border border-solid border-slate-100">
       <Content />
       <VCopyright class="not-prose" />
     </article>
