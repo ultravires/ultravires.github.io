d41f2bcbd95c06ce0562a67bd1da84ee3f3f9bf7
ultravires <x555666777@qq.com>
2024-05-20 17:53:42 +0800

    Update

--- docs/.vitepress/theme/components/VRandomArticle.vue
+++ docs/.vitepress/theme/components/VRandomArticle.vue
@@ -2,15 +2,42 @@
 import { useRouter } from 'vitepress';
 import VIconDice from './icons/VIconDice.vue';
 import { data as posts } from '../posts.data.js';
+import { computed, ref } from 'vue';
 
 const router = useRouter();
 
+const duration = ref(1); // s
+const rotating = ref(false);
+
+const diceAnimationDuration = computed(() => {
+  return duration.value + 's';
+});
+
 const visiteRandomArticle = () => {
+  rotating.value = true;
+  setTimeout(() => {
+    rotating.value = false;
+  }, duration.value * 1000);
   const random = Math.floor(Math.random() * posts.length);
   router.go(posts[random].url);
 };
 </script>
 
 <template>
-  <VIconDice title="随机访问该站文章" @click="visiteRandomArticle" />
+  <VIconDice :class="['dice', { rotating }]" title="随机访问该站文章" @click="visiteRandomArticle" />
 </template>
+
+<style scoped>
+.dice.rotating {
+  animation: rotate v-bind(diceAnimationDuration) cubic-bezier(0.075, 0.82, 0.165, 1);
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotate(0);
+  }
+  100% {
+    transform: rotate(720deg);
+  }
+}
+</style>
