4484a0b278b8ee91e1e329daad9be8074d976282
ultravires <x555666777@qq.com>
2024-05-22 10:18:05 +0800

    Client only

--- docs/.vitepress/theme/layout/index.vue
+++ docs/.vitepress/theme/layout/index.vue
@@ -2,22 +2,15 @@
 import { useData } from 'vitepress';
 import HomeLayout from './HomeLayout.vue';
 import DocLayout from './DocLayout.vue';
-import { isSSR } from '../support/utils';
 
 const { frontmatter } = useData();
 </script>
 
 <template>
-  <ClientOnly v-if="isSSR()">
+  <ClientOnly>
     <section class="dark:bg-neutral-950 dark:text-white min-h-screen bg-neutral-50 text-black">
       <HomeLayout v-if="frontmatter.layout === 'home'" />
       <DocLayout v-else />
     </section>
   </ClientOnly>
-  <template v-else>
-    <section class="dark:bg-neutral-950 dark:text-white min-h-screen bg-neutral-50 text-black">
-      <HomeLayout v-if="frontmatter.layout === 'home'" />
-      <DocLayout v-else />
-    </section>
-  </template>
 </template>
