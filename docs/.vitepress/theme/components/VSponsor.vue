b271de6d6bc1b3b29f66bb07375aeeb059e16852
ultravires <x555666777@qq.com>
2024-06-10 19:09:33 +0800

    Update

--- docs/.vitepress/theme/components/VSponsor.vue
+++ docs/.vitepress/theme/components/VSponsor.vue
@@ -12,7 +12,16 @@ import VIconHandHeart from './icons/VIconHandHeart.vue';
     <div class="peer-hover:block peer-hover:opacity-100 peer-hover:translate-y-0 peer-hover:pointer-events-auto hover:block hover:opacity-100 hover:translate-y-0 hover:pointer-events-auto absolute bottom-full opacity-0 translate-y-4 pointer-events-none left-1/2 -translate-x-1/2 z-10 transition-all duration-300">
       <div class="dark:bg-neutral-900 dark:border-neutral-600 p-8 mb-4 w-max bg-white border border-solid border-neutral-200 rounded-lg ">
         <p class="mb-2 whitespace-nowrap text-center text-red-500 text-lg font-medium">感谢你赐予我前进的动力！</p>
-        <img class="w-52 h-52 object-contain" :src="withBase('/wechat.jpeg')" />
+        <div class="flex gap-4">
+          <div class="flex flex-col justify-center">
+            <img class="w-40 h-40 object-contain" :src="withBase('/WeChatPay.jpg')" />
+            <div class="text-center">微信</div>
+          </div>
+          <div class="flex flex-col justify-center">
+            <img class="w-40 h-40 object-contain" :src="withBase('/AliPay.jpg')" />
+            <div class="text-center">支付宝</div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
