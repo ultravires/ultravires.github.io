1b9e258876062becfdb083ed6ec9acf50092ba9b
ultravires <x555666777@qq.com>
2024-07-03 17:41:57 +0800

    Update

--- docs/.vitepress/theme/components/VPostList.vue
+++ docs/.vitepress/theme/components/VPostList.vue
@@ -1,18 +1,27 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
 import { data as posts } from '../posts.data.js';
 import VPost from '../components/VPost.vue';
 import VPostCard from '../components/VPostCard.vue';
+
+const type = ref('card'); // 'card' | 'list'
 </script>
 
 <template>
-  <ul class="flex flex-wrap">
+  <ul class="flex flex-wrap w-full">
     <li
       v-for="post in posts"
       :key="post.title"
-      class="max-md:w-full max-md:mr-0 even:mr-0 w-[calc(calc(100%-1rem)/2)] mr-4 mb-4"
+      :class="[
+        {
+          'max-md:w-full max-md:mr-0 even:mr-0 w-[calc(calc(100%-1rem)/2)] mr-4 mb-4':
+            type === 'card',
+        },
+        { 'w-full mb-8': type === 'list' },
+      ]"
     >
-      <!-- <VPost :post="post" /> -->
-      <VPostCard :post="post" />
+      <VPost v-if="type === 'list'" :post="post" />
+      <VPostCard v-else-if="type === 'card'" :post="post" />
     </li>
   </ul>
 </template>
