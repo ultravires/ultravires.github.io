56db662049a6ee8b0488d37d151778eacd72a27b
ultravires <x555666777@qq.com>
2024-07-03 17:58:32 +0800

    Update

--- docs/.vitepress/theme/components/VWave.vue
+++ docs/.vitepress/theme/components/VWave.vue
@@ -5,9 +5,20 @@ const { frontmatter } = useData();
 </script>
 
 <template>
-  <svg v-show="frontmatter?.banner?.wave" class="waves-svg max-md:hidden" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+  <svg
+    v-show="frontmatter?.banner?.wave"
+    class="waves-svg max-md:hidden"
+    xmlns="http://www.w3.org/2000/svg"
+    xlink="http://www.w3.org/1999/xlink"
+    viewBox="0 24 150 28"
+    preserveAspectRatio="none"
+    shape-rendering="auto"
+  >
     <defs>
-      <path id="gentle-wave" d="M -160 44 c 30 0 58 -18 88 -18 s 58 18 88 18 s 58 -18 88 -18 s 58 18 88 18 v 44 h -352 Z"></path>
+      <path
+        id="gentle-wave"
+        d="M -160 44 c 30 0 58 -18 88 -18 s 58 18 88 18 s 58 -18 88 -18 s 58 18 88 18 v 44 h -352 Z"
+      ></path>
     </defs>
     <g class="parallax">
       <use href="#gentle-wave" x="48" y="0"></use>
@@ -31,32 +42,33 @@ const { frontmatter } = useData();
   --wave-background: #0a0a0a;
 }
 
-.parallax>use {
-  animation: move-forever 30s cubic-bezier(.55,.5,.45,.5) infinite;
+.parallax > use {
+  animation: move-forever 30s cubic-bezier(0.55, 0.5, 0.45, 0.5) infinite;
+  transition: all 0.3s cubic-bezier(0.4, 0, 0.2, 1);
 }
 
-.parallax>use:nth-child(1) {
+.parallax > use:nth-child(1) {
   animation-delay: -2s;
   animation-duration: 7s;
   fill: var(--wave-background);
-  opacity: .5;
+  opacity: 0.5;
 }
 
-.parallax>use:nth-child(2) {
+.parallax > use:nth-child(2) {
   animation-delay: -3s;
   animation-duration: 10s;
   fill: var(--wave-background);
-  opacity: .6;
+  opacity: 0.6;
 }
 
-.parallax>use:nth-child(3) {
+.parallax > use:nth-child(3) {
   animation-delay: -4s;
   animation-duration: 13s;
   fill: var(--wave-background);
-  opacity: .7;
+  opacity: 0.7;
 }
 
-.parallax>use:nth-child(4) {
+.parallax > use:nth-child(4) {
   animation-delay: -5s;
   animation-duration: 20s;
   fill: var(--wave-background);
@@ -64,10 +76,10 @@ const { frontmatter } = useData();
 
 @keyframes move-forever {
   from {
-    transform: translate3d(-90px,0,0);
+    transform: translate3d(-90px, 0, 0);
   }
   to {
-    transform: translate3d(85px,0,0);
+    transform: translate3d(85px, 0, 0);
   }
 }
 </style>
--- docs/.vitepress/theme/layout/DocLayout.vue
+++ docs/.vitepress/theme/layout/DocLayout.vue
@@ -19,12 +19,12 @@ import VProfileCard from '../components/VProfileCard.vue';
         </div>
       </aside>
       <div
-        class="dark:bg-neutral-900 dark:border-neutral-700 dark:max-md:bg-transparent max-md:px-6 max-md:px-6 max-md:border-0 max-md:bg-transparent order-1 p-12 w-full mx-auto bg-white rounded-md border border-solid border-neutral-200 overflow-auto"
+        class="dark:bg-neutral-900 dark:border-neutral-700 dark:max-md:bg-transparent max-md:px-6 max-md:px-6 max-md:border-0 max-md:bg-transparent order-1 p-12 w-full mx-auto bg-white rounded-md border border-solid border-neutral-200 overflow-auto transition-all duration-300"
       >
         <article
           ref="docRef"
           id="article"
-          class="VPDoc lg:prose-xl dark:prose-invert dark:prose-pre:border-neutral-700 prose-pre:border prose-pre:border-solid prose-pre:border-neutral-200 prose-code:px-2 prose-code:py-1 prose-code:bg-primary prose-code:bg-opacity-10 prose-code:text-primary prose-code:rounded-md prose-blockquote:not-italic prose-blockquote:font-normal prose-table:block prose-table:overflow-x-auto prose-th:border prose-th:border-solid prose-td:border prose-td:border-solid max-w-none prose prose-lg prose-neutral"
+          class="VPDoc lg:prose-xl dark:prose-invert dark:prose-pre:border-neutral-700 prose-pre:border prose-pre:border-solid prose-pre:border-neutral-200 prose-code:px-2 prose-code:py-1 prose-code:bg-primary prose-code:bg-opacity-10 prose-code:text-primary prose-code:rounded-md prose-blockquote:not-italic prose-blockquote:font-normal prose-table:block prose-table:overflow-x-auto prose-th:border prose-th:border-solid prose-td:border prose-td:border-solid max-w-none prose prose-lg prose-neutral transition-all duration-300"
         >
           <Content />
         </article>
