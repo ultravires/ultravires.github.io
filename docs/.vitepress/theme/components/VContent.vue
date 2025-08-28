e9634a10bccd072a48b5840b3879bae2ba6d97a0
向成渝 <xiangchengyu@datagrand.com>
2025-08-28 14:06:24 +0800

    fix: 修复文章页面样式问题

--- docs/.vitepress/theme/components/VContent.vue
+++ docs/.vitepress/theme/components/VContent.vue
@@ -4,7 +4,7 @@ import VDocFooter from '@theme/components/VDocFooter.vue';
 </script>
 
 <template>
-  <div class="@8xl:max-w-[87.5rem] max-md:px-2 px-8 mx-auto">
+  <div class="@8xl:max-w-[87.5rem] max-md:px-2 px-8 mx-auto overflow-x-hidden">
     <div
       class="order-1 w-full p-12 mx-auto overflow-auto transition-all duration-300 bg-white border border-solid rounded-md dark:bg-neutral-900 dark:max-md:bg-transparent max-md:px-6 max-md:border-0 max-md:bg-transparent border-line"
     >
