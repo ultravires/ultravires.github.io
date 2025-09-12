8c68cc2b2f465715bca59e6e42a6e1980ed7b688
向成渝 <xiangchengyu@datagrand.com>
2025-09-12 13:21:05 +0800

    feat: algoliasearch

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -9,52 +9,60 @@ import { withMermaid } from 'vitepress-plugin-mermaid';
 import type { CustomTheme } from './theme/types/custom-vitepress-theme.d.js';
 
 // https://vitepress.vuejs.org/config/app-configs
-export default defineConfigWithTheme<CustomTheme>(withMermaid({
-  lang: zh.lang,
-  title: zh.title,
-  description: zh.description,
-  srcDir: 'src',
-  cleanUrls: true,
-  buildEnd: genFeed,
-  markdown: zh.markdown,
-  themeConfig: zh.themeConfig,
-  lastUpdated: true,
-  ignoreDeadLinks: true,
-  head: [
-    ['meta', { name: 'author', content: '向成渝' }],
-    [
-      'meta',
-      {
-        name: 'viewport',
-        content:
-          'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0',
-      },
+export default defineConfigWithTheme<CustomTheme>(
+  withMermaid({
+    lang: zh.lang,
+    title: zh.title,
+    description: zh.description,
+    srcDir: 'src',
+    cleanUrls: true,
+    buildEnd: genFeed,
+    markdown: zh.markdown,
+    themeConfig: zh.themeConfig,
+    lastUpdated: true,
+    ignoreDeadLinks: true,
+    head: [
+      ['meta', { name: 'author', content: '向成渝' }],
+      [
+        'meta',
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
+        }
+      ],
+      [
+        'meta',
+        {
+          name: 'algolia-site-verification',
+          content: '338A0D7313BC8277'
+        }
+      ],
+      [
+        'link',
+        {
+          rel: 'preconnect',
+          href: 'https://AKWVEI7J63-dsn.algolia.net',
+          crossorigin: ''
+        }
+      ]
     ],
-    [
-      'link',
-      {
-        rel: 'preconnect',
-        href: 'https://AKWVEI7J63-dsn.algolia.net',
-        crossorigin: '',
+    vite: {
+      define: {
+        // 启用生产环境构建下激活不匹配的详细警告
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true'
       },
-    ]
-  ],
-  vite: {
-    define: {
-      // 启用生产环境构建下激活不匹配的详细警告
-      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true',
-    },
-    build: {
-      cssCodeSplit: false
-    },
-    plugins: [
-      svgLoader({
-        svgo: false,
-        defaultImport: 'component'
-      }),
-      tailwindcss(),
-      vueDevTools(),
-      VueMcp()
-    ]
-  }
-}));
+      build: {
+        cssCodeSplit: false
+      },
+      plugins: [
+        svgLoader({
+          svgo: false,
+          defaultImport: 'component'
+        }),
+        tailwindcss(),
+        vueDevTools(),
+        VueMcp()
+      ]
+    }
+  })
+);
--- docs/.vitepress/config/zh.ts
+++ docs/.vitepress/config/zh.ts
@@ -15,7 +15,7 @@ export const zh = defineConfigWithTheme<CustomTheme>({
     aside: 'left',
     search: {
       provider: 'algolia',
-      options: algoliaSearchOptions(),
+      options: algoliaSearchOptions()
     }
   },
 
@@ -23,12 +23,12 @@ export const zh = defineConfigWithTheme<CustomTheme>({
     codeTransformers: [transformerTwoslash()],
     theme: {
       light: 'dracula-soft',
-      dark: 'github-dark-default',
+      dark: 'github-dark-default'
     },
     lineNumbers: true,
     math: true, // require `markdown-it-mathjax3`
     image: {
-      lazyLoading: true,
+      lazyLoading: true
     },
     codeCopyButtonTitle: '复制代码',
     container: {
@@ -36,9 +36,9 @@ export const zh = defineConfigWithTheme<CustomTheme>({
       warningLabel: '警告',
       dangerLabel: '危险',
       infoLabel: '信息',
-      detailsLabel: '详细信息',
-    },
-  },
+      detailsLabel: '详细信息'
+    }
+  }
 });
 
 function nav(): DefaultTheme.NavItem[] {
@@ -46,7 +46,7 @@ function nav(): DefaultTheme.NavItem[] {
     {
       text: '友链',
       link: '/link/',
-      activeMatch: `^/link/`,
+      activeMatch: `^/link/`
     },
     {
       text: '专栏',
@@ -55,64 +55,64 @@ function nav(): DefaultTheme.NavItem[] {
         {
           text: '程序人生',
           link: '/life/',
-          activeMatch: `^/life/`,
+          activeMatch: `^/life/`
         },
         {
           text: '算法题解',
           link: '/algorithm/',
-          activeMatch: `^/algorithm/`,
+          activeMatch: `^/algorithm/`
         },
         {
           text: '前端领域',
           link: '/front-end/',
-          activeMatch: `^/front-end/`,
+          activeMatch: `^/front-end/`
         },
         {
           text: '后端领域',
           link: '/back-end/',
-          activeMatch: `^/back-end/`,
+          activeMatch: `^/back-end/`
         },
         {
           text: '人工智能',
           link: '/ai/',
-          activeMatch: `^/ai/`,
-        },
-      ],
+          activeMatch: `^/ai/`
+        }
+      ]
     },
     {
       text: '关于',
       link: '/about/',
-      activeMatch: '/about/',
-    },
+      activeMatch: '/about/'
+    }
   ];
 }
 
 function footer(): DefaultTheme.Footer {
   return {
     message: '基于 MIT 许可发布',
-    copyright: `版权所有 &copy; 2024-${new Date().getFullYear()} 向成渝`,
+    copyright: `版权所有 &copy; 2024-${new Date().getFullYear()} 向成渝`
   };
 }
 
 function algoliaSearchOptions(): DefaultTheme.AlgoliaSearchOptions {
   return {
-    appId: 'AKWVEI7J63',
+    appId: 'NWG447M0CU',
     indexName: 'ultravires.github.io',
-    apiKey: '7a14faa733a46057d98726e018afe153',
+    apiKey: 'c2286d75787e2cba5a32a5b3a0615f26',
     locales: {
       zh: {
         placeholder: '搜索文档',
         translations: {
           button: {
             buttonText: '搜索文档',
-            buttonAriaLabel: '搜索文档',
+            buttonAriaLabel: '搜索文档'
           },
           modal: {
             searchBox: {
               resetButtonTitle: '清除查询条件',
               resetButtonAriaLabel: '清除查询条件',
               cancelButtonText: '取消',
-              cancelButtonAriaLabel: '取消',
+              cancelButtonAriaLabel: '取消'
             },
             startScreen: {
               recentSearchesTitle: '搜索历史',
@@ -120,27 +120,27 @@ function algoliaSearchOptions(): DefaultTheme.AlgoliaSearchOptions {
               saveRecentSearchButtonTitle: '保存至搜索历史',
               removeRecentSearchButtonTitle: '从搜索历史中移除',
               favoriteSearchesTitle: '收藏',
-              removeFavoriteSearchButtonTitle: '从收藏中移除',
+              removeFavoriteSearchButtonTitle: '从收藏中移除'
             },
             errorScreen: {
               titleText: '无法获取结果',
-              helpText: '你可能需要检查你的网络连接',
+              helpText: '你可能需要检查你的网络连接'
             },
             footer: {
               selectText: '选择',
               navigateText: '切换',
               closeText: '关闭',
-              searchByText: '搜索提供者',
+              searchByText: '搜索提供者'
             },
             noResultsScreen: {
               noResultsText: '无法找到相关结果',
               suggestedQueryText: '你可以尝试查询',
               reportMissingResultsText: '你认为该查询应该有结果？',
-              reportMissingResultsLinkText: '点击反馈',
-            },
-          },
-        },
-      },
-    },
+              reportMissingResultsLinkText: '点击反馈'
+            }
+          }
+        }
+      }
+    }
   };
 }
