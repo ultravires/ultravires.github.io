1e05927128a0d29d554e7346a5a9d95ec7616da0
向成渝 <xiangchengyu@datagrand.com>
2025-11-14 18:44:07 +0800

    update

--- docs/.vitepress/theme/layout/AboutLayout.vue
+++ docs/.vitepress/theme/layout/AboutLayout.vue
@@ -2,7 +2,7 @@
 import { Icon } from '@iconify/vue';
 import PageContainer from '@theme/components/PageContainer.vue';
 import { getAvatarUrl } from '@theme/composables/useGravatar';
-import { useData } from 'vitepress';
+import { Content, useData } from 'vitepress';
 
 const { frontmatter } = useData();
 
@@ -16,7 +16,7 @@ const user = frontmatter.value?.self ?? {};
       <img
         :src="getAvatarUrl(user.email)"
         alt="Avatar"
-        class="block h-16 w-16 rounded-full"
+        class="block h-16 w-16 rounded-full shadow"
       />
       <div class="mt-6">
         <h2 class="my-4 text-4xl font-semibold">前端工程师</h2>
@@ -27,7 +27,7 @@ const user = frontmatter.value?.self ?? {};
           v-for="link in user.links"
           :href="link.link"
           target="_blank"
-          class="inline-block text-gray-500 hover:text-gray-700"
+          class="inline-block text-gray-600 hover:text-black"
         >
           <Icon
             :icon="link.icon"
@@ -37,32 +37,40 @@ const user = frontmatter.value?.self ?? {};
       </div>
     </div>
 
-    <section class="max-w-96 rounded-2xl bg-white p-4 shadow">
-      <h3 class="flex items-center">
-        <Icon
-          icon="mdi:briefcase-variant-outline"
-          class="text-2xl"
-        />
-        <span class="text-xl font-extrabold">工作经历</span>
-      </h3>
-      <div>
-        <div
-          v-for="work in user.works"
-          class="flex w-full items-center gap-2 p-4"
-        >
-          <img
-            class="h-12 w-12 rounded-full object-contain p-2 shadow"
-            :src="work.logo"
+    <div class="mt-12 flex w-full gap-4">
+      <div class="flex-1">
+        <Content class="prose max-w-none" />
+      </div>
+
+      <div
+        class="max-w-96 shrink-0 rounded-2xl border border-neutral-100/10 bg-white p-4 shadow"
+      >
+        <h3 class="flex items-center">
+          <Icon
+            icon="mdi:briefcase-variant-outline"
+            class="text-2xl"
           />
-          <div class="flex-1">
-            <p class="font-bold">{{ work.company }}</p>
-            <div class="mb-2 flex justify-between">
-              <span class="text-gray-600">{{ work.position }}</span>
-              <span class="text-gray-400">{{ work.time }}</span>
+          <span class="text-xl font-extrabold">工作经历</span>
+        </h3>
+        <div>
+          <div
+            v-for="work in user.works"
+            class="flex w-full items-center gap-2 p-4"
+          >
+            <img
+              class="h-12 w-12 rounded-full border border-neutral-100/20 object-contain p-2 shadow"
+              :src="work.logo"
+            />
+            <div class="flex-1">
+              <p class="font-bold">{{ work.company }}</p>
+              <div class="mb-2 flex justify-between">
+                <span class="text-gray-600">{{ work.position }}</span>
+                <span class="text-gray-400">{{ work.time }}</span>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   </PageContainer>
 </template>
